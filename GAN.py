fc711d52173bbff9f33890a9800391fc45558eca
周昌福 <2916661445@qq.com>
2018-04-16 14:40:43 +0800

    GAN test

--- GAN.py
+++ GAN.py
@@ -8,42 +8,44 @@ def GAN():
     with tf.variable_scope("Generator"):
         x=tf.placeholder(shape=[1,1],dtype=tf.float32)
         hidden=tf.layers.dense(inputs=x,units=20)
-        gPredict=tf.layers.dense(hidden,1)
+        gPredict=tf.layers.dense(hidden,1,activation=tf.nn.sigmoid)
     with tf.variable_scope("Discriminator"):
         #fake discriminate result
-        dDiscriminate=tf.layers.dense(gPredict,1)
+        D1=tf.layers.dense(gPredict,10)
+        fakeArtD=tf.layers.dense(gPredict,1,activation=tf.nn.sigmoid)
 
         #the real result
         y=tf.placeholder(shape=[1,1],dtype=tf.float32)
+        D2=tf.layers.dense(y,10)
+        realArtD=tf.layers.dense(y,1,activation=tf.nn.sigmoid)
 
         #define D loss
-        dLoss=-tf.sigmoid(tf.losses.mean_squared_error(y,dDiscriminate))
+    dLoss=-(tf.log(1-fakeArtD)*0.999+0.001*tf.log(realArtD))
 
-        gLoss=tf.losses.mean_squared_error(y,dDiscriminate)
+    gLoss=tf.log(1-fakeArtD)
 
     gTrainStep=tf.train.AdamOptimizer().minimize(gLoss,var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope="Generator"))
     dTrainStep=tf.train.AdamOptimizer().minimize(dLoss,var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope="Discriminator"))
     with tf.Session() as sess:
         init=tf.global_variables_initializer()
         sess.run(init)
-        xValue=np.linspace(-10,10,1000)
+        xValue=np.linspace(-10,10,10)
         yValue=np.sin(xValue)
-        for i in range(10000):
+        for ii in range(10000):
             simulinkValueList=[]
             for i in range(len(xValue)):
-                sess.run(gTrainStep,feed_dict={x:[[xValue[i]]],y:[[yValue[i]]]})
-
-                sess.run(dTrainStep, feed_dict={x: [[xValue[i]]], y: [[yValue[i]]]})
+                for jj in range(10):
+                    sess.run([gTrainStep,dTrainStep],feed_dict={x:[[xValue[i]]],y:[[yValue[i]]]})
 
                 simulinkValueList.append(sess.run(gPredict,feed_dict={x:[[xValue[i]]]})[0][0])
-            if (i+1)%1==0:
-                plt.cla()
-                plt.plot(xValue, yValue)
-                plt.legend("generator")
-                plt.plot(xValue,simulinkValueList)
-                plt.legend("discriminator")
-                plt.pause(0.01)
-plt.show()
+
+                #print("loss:"+str(np.abs(np.sum(np.abs(np.array(simulinkValueList)-np.array(yValue))))))
+            plt.cla()
+            plt.plot(xValue, yValue)
+            plt.legend("generator")
+            plt.plot(xValue,simulinkValueList)
+            plt.legend("discriminator")
+            plt.pause(0.01)
 GAN()
 
 
